f69d6f98d1bc756881ce9584262441b39b196c7e
Philipp Weiss <p.weiss85@gmx.de>
2022-10-09 17:55:38 +0200

    finish exericse 1-4

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,3 +1,16 @@
+import {getDomainLocale} from 'next/dist/shared/lib/router/router';
+import {getAllUsers, getUserById} from '../../../services/userService';
+import {deleteUser} from '../../../services/userService';
+
 export default async function handler(request, response) {
+  const {id} = request.query;
+
+  if (request.method === 'GET') {
+    const data = await getUserById(id);
+    return response.status(200).json(data);
+  } else if (request.method === 'DELETE') {
+    const data = await deleteUser(id);
+    return response.status(201).json(data);
+  }
   res.status(403).json({message: 'Error: request method not allowed.'});
 }
--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,8 +1,19 @@
+import {getAllUsers, createUser} from '../../../services/userService';
+
 export default async function handler(request, response) {
   if (request.method === 'GET') {
-    const data = [];
+    const data = await getAllUsers();
     return response.status(200).json(data);
-  }
+  } else if (request.method === 'POST') {
+    const newData = JSON.parse(request.body);
+    const newProduct = await createUser(
+      newData.name,
+      newData.gender,
+      newData.email,
+    );
+    console.log(newProduct);
+    return response.status(201).json(newProduct);
 
-  res.status(403).json({message: 'Error: request method not allowed.'});
+    res.status(403).json({message: 'Error: request method not allowed.'});
+  }
 }
